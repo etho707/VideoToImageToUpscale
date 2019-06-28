68e4fde256f1952e78e9f299efe1a58f9c3a94d0
mihinus <drxkane@gmail.com>
2019-06-28 20:58:17 +0300

    co mit

---
+++ MrConsole/MrConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hell no word");
+            var vr = new VideoReader.VideoReader();
+            vr.Pipis();
+            Console.WriteLine("ok word");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VideoReader/VideoReader/Class1.cs
@@ -0,0 +1,31 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using MediaToolkit.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoReader
+{
+    public class VideoReader
+    {
+        public void Pipis()
+        {
+            using (var engine = new Engine())
+            {
+                var mp4 = new MediaFile { Filename = @"A:\Tors\Serial_Experiments_Lain_BDRip_480p_Hi10p_[AnimeCeh]\Serial_Experiments_Lain_01_BDRip_480p_Hi10p_[AnimeCeh].mkv" };
+
+                engine.GetMetadata(mp4);
+
+                for (int x=0; x < Convert.ToInt32(mp4.Metadata.Duration.TotalSeconds); x++) { 
+                    var options = new ConversionOptions { Seek = TimeSpan.FromSeconds(x) };
+                    var outputFile = new MediaFile { Filename = string.Format("{0}\\image-{1}.jpeg", @"A:\Tors\Serial_Experiments_Lain_BDRip_480p_Hi10p_[AnimeCeh]\baka", x) };
+                    engine.GetThumbnail(mp4, outputFile, options);
+                };
+                
+            }
+        }
+    }
+}
